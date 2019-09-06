0ff95b2a69a53bf9ecb53d1f8759b232d4c37f21
kobasou <kobasou.205@gmail.com>
2019-09-06 12:31:37 +0900

    サブマイコンとの通信処理を実装

--- Ateam_AR_Main/inc/sequence.hpp
+++ Ateam_AR_Main/inc/sequence.hpp
@@ -10,24 +10,34 @@
 
 #include "systick.hpp"
 #include "auto_running.hpp"
-
+#include "control_area_network.hpp"
 #include "board_io.hpp"
 
 class Sequence
 {
 private:
+	static constexpr uint8_t arm_ctrl_board_add_ = 0x05;
+
 	SysTick_Interrupt systick_;
 	IO_sigPins<ioName::sig0, ioState::input, pinPullDirection::up> start_switch_;
 
 	AutoRunning auto_running_;
+	CAN_Interrupt can_interrupt_;
+	ControlAreaNetwork can_interface_;
 
+	uint8_t received_msg_ = 0;
+
+	void canReceiveMsg(const CanRxMsg& receive_msg);
 	void runSequence();
+	inline void sendCmd(uint8_t send_cmd){ can_interface_.sendData(&send_cmd, 1, arm_ctrl_board_add_); }
 
 public:
 	Sequence() = delete;
 	Sequence(UART& gyro_uart_interface, ControlAreaNetwork& can_interface) :
 		systick_( [&](){ runSequence(); } ),
-		auto_running_(gyro_uart_interface, can_interface){ }
+		auto_running_(gyro_uart_interface, can_interface),
+		can_interrupt_( [&](const CanRxMsg& receive_msg){ canReceiveMsg(receive_msg); } ),
+		can_interface_(can_interface){ }
 
 	position<float> pos(){ return { auto_running_.pos().posX(), auto_running_.pos().posY(), auto_running_.pos().posTheta()}; }
 };
--- Ateam_AR_Main/src/sequence.cpp
+++ Ateam_AR_Main/src/sequence.cpp
@@ -16,6 +16,7 @@ void Sequence::runSequence()
 	{
 	case 0:
 		auto_running_.dead_reckoning_.setPos(-600.0f, 1800.0f, M_PI);
+		received_msg_ = 0;
 		if(!start_switch_.readNowState())
 		{
 			++now_sequence_num_;
@@ -23,24 +24,93 @@ void Sequence::runSequence()
 		break;
 
 	case 1:
+		sendCmd(0x01);
+		++now_sequence_num_;
+		break;
+
+	case 2:
 		if(auto_running_.pointControl(0))
 		{
 			++now_sequence_num_;
 		}
 		break;
 
-	case 2:
+	case 3:
+		if(received_msg_ == 0x01)
+		{
+			sendCmd(0x02);
+			++now_sequence_num_;
+		}
+		break;
+
+	case 4:
+		if(received_msg_ == 0x02)
+		{
+			sendCmd(0x03);
+			++now_sequence_num_;
+		}
+		break;
+
+	case 5:
 		if(auto_running_.pointControl(1))
 		{
 			++now_sequence_num_;
 		}
 		break;
 
-	case 3:
-		if(auto_running_.pointControl(2))
+	case 6:
+		if(received_msg_ == 0x03)
+		{
+			sendCmd(0x04);
+			++now_sequence_num_;
+		}
+		break;
+
+	case 7:
+		if(received_msg_ == 0x04)
+		{
+			sendCmd(0x05);
+			++now_sequence_num_;
+		}
+		break;
+
+	case 8:
+		if(auto_running_.pointControl(2) && received_msg_ == 0x05)
+		{
+			sendCmd(0x06);
+			++now_sequence_num_;
+		}
+		break;
+
+	case 9:
+		if(received_msg_ == 0x06)
+		{
+			sendCmd(0x07);
+			++now_sequence_num_;
+		}
+		break;
+
+	case 10:
+		if(received_msg_ == 0x07)
+		{
+			sendCmd(0x08);
+			++now_sequence_num_;
+		}
+		break;
+
+	case 11:
+		if(auto_running_.pointControl(3))
 		{
 			++now_sequence_num_;
 		}
 		break;
+	}
+}
+
+void Sequence::canReceiveMsg(const CanRxMsg& receive_msg)
+{
+	if(receive_msg.Data[0] == arm_ctrl_board_add_)
+	{
+		received_msg_ = receive_msg.Data[1];
 	}
 }

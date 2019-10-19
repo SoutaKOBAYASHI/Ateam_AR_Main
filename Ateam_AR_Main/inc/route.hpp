cac787551e29e255ed133365dc611252c76bc9a4
kobasou <kobasou.205@gmail.com>
2019-10-20 01:34:01 +0900

    ルートまとめ

--- Ateam_AR_Main/inc/route.hpp
+++ Ateam_AR_Main/inc/route.hpp
@@ -12,8 +12,23 @@
 #include <array>
 #include <vector>
 
-#include "route_path_1.hpp"
-#include "route_path_2.hpp"
+#if (ZONE_IS_RED)
+#include "red_zone/qualyfying/route_path_1.hpp"
+#include "red_zone/qualyfying/route_path_2.hpp"
+#include "red_zone/qualyfying/route_path_3.hpp"
+#include "red_zone/qualyfying/route_path_4.hpp"
+#include "red_zone/qualyfying/route_path_5.hpp"
+#include "red_zone/qualyfying/route_path_6.hpp"
+#include "red_zone/qualyfying/route_path_7.hpp"
+#else
+#include "blue_zone/qualyfying/route_path_1.hpp"
+#include "blue_zone/qualyfying/route_path_2.hpp"
+#include "blue_zone/qualyfying/route_path_3.hpp"
+#include "blue_zone/qualyfying/route_path_4.hpp"
+#include "blue_zone/qualyfying/route_path_5.hpp"
+#include "blue_zone/qualyfying/route_path_6.hpp"
+#include "blue_zone/qualyfying/route_path_7.hpp"
+#endif
 
 #ifndef M_PI
 #define M_PI		3.14159265358979323846
@@ -22,10 +37,15 @@
 using Tpos = float;
 using Tvel = uint32_t;
 
-const std::array<std::vector<pathPoint<float>>, 2> running_path =
+const std::array<std::vector<pathPoint<float>>, 7> running_path =
 {
 		route_path_1,
-		route_path_2
+		route_path_2,
+		route_path_3,
+		route_path_4,
+		route_path_5,
+		route_path_6,
+		route_path_7
 };
 
 #endif /* ROUTE_HPP_ */
